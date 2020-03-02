27df351dbc309642e2a959bb20e5939f14cfe5f8
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-02 03:18:24 -0600

    service-worker install logic

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ console.log("Hello from your service worker!");
 const FILES_TO_CACHE = [
     '/',
     './index.html',
+    './facvicon.ico',
     './index.js',
     './style.css',
     './manifest.webmanifest',
@@ -12,3 +13,18 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = 'static-cache-v2'
 const DATA_CACHE_NAME = 'data-cache-v1'
+
+// install using event.waitUntil()
+// this method keeps service-worker in install phase until
+// promise is resolved
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('files pre-cached successfully')
+                return cache.addAll(FILES_TO_CACHE)
+            })
+    )
+    self.skipWaiting()
+});
+
