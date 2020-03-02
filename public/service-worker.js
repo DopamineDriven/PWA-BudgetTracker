5d98abfac60871378b79e134ad653a6e8e9e520e
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-02 03:37:06 -0600

    activate event written for service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,9 +18,11 @@ const DATA_CACHE_NAME = 'data-cache-v1'
 // this method keeps service-worker in install phase until
 // promise is resolved
 self.addEventListener('install', event => {
+    // wait until cache is opened
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
+                // then pre-cache all files to cache
                 console.log('files pre-cached successfully')
                 return cache.addAll(FILES_TO_CACHE)
             })
@@ -28,3 +30,30 @@ self.addEventListener('install', event => {
     self.skipWaiting()
 });
 
+
+// event listener for activation
+self.addEventListener('activate', event => {
+    // wait until keys retrieved from cache
+    event.waitUntil(
+        caches.keys()
+            // caches is an obj containing all caches
+            // keys() method retrieves all keys
+            .then(keyList => {
+                // list of keys passed in, returning a promise array
+                return Promise.all(
+                    // build new array by modifying each element in key list
+                    keyList.map(key => {
+                        // current iteration of keylist passed in
+                        // checks if key is not equal to cache names
+                        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                            console.log(`removing old cache data ${key}`)
+                            // if data !== names, delete old name
+                            return caches.delete(key)
+                        }
+                    })
+                )
+
+            })
+    )
+    self.clients.claim()
+});
