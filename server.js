3465ae0199a639ea38a029b74756a1724588d09c
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-03 01:14:33 -0600

    server updates, running locally

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs")
 const compression = require("compression");
+const path = require("path");
 
 const PORT = process.env.PORT || 1234;
 
@@ -15,6 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// connect to database
 mongoose.connect("mongodb://localhost/PWA-BudgetTracker", {
     useNewUrlParser: true, 
     useFindAndModify: false,
@@ -22,9 +25,21 @@ mongoose.connect("mongodb://localhost/PWA-BudgetTracker", {
     useUnifiedTopology: true
 });
 
+// setting up mongo db
+const databaseURL = process.env.MONGODB_URI || "PWA-BudgetTracker";
+const collections = ["PWA-BudgetTracker"]
+
+// reference to database
+const db = mongojs(databaseURL, collections)
+
+// if error with database, throw
+db.on("error", error => {
+  console.log(`DB error: ${error}`)
+});
+
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on PORT ${PORT}!`);
 });
